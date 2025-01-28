f42f3ba34ac2eb1bb3ca8f998ccc2ab7c3ff62f0
mohamed-wahbi <wahbisj@gmail.com>
2025-01-28 18:27:26 +0100

    creatio du home page qui doit étre le depare de notre platforme, avec la configuration du routting system.

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='homeComp'>
+      <img src='https://www.tuntrust.tn/themes/medianet/image/png/logo_fr.png' alt='logo' className='logoImg' />
+      <p className='description' >Cette plateforme permet ainsi de gagner en efficacité, de réduire les erreurs humaines et de garantir une gestion proactive des compétences et des certifications à l’échelle de l’entreprise. Elle constitue un véritable levier pour la performance, en alignant les ressources humaines avec les objectifs à long terme de l'organisation.</p>
+      <button className='getStartBtn'>Commencer</button>
+      <p className='GoLoginBtn'>Vous avez déjà un compte ? <span>Connectez-vous maintenant...</span></p>
+    </div>
+  )
+}
+
+export default Home
